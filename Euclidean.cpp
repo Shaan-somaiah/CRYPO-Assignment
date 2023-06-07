4174d47dbeab4e11426dec4a0bac0f74877dd0fd
Shaan-somaiah <108606284+Shaan-somaiah@users.noreply.github.com>
2023-06-07 14:54:57 +0530

    Update Euclidean.cpp

--- Euclidean.cpp
+++ Euclidean.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int extended_euclidean_algorithm(int a, int b, int& x, int& y) {
     if (b == 0) {
         x = 1;
         y = 0;
         return a;
     }
-
     int x1, y1;
     int gcd = extended_euclidean_algorithm(b, a % b, x1, y1);
     x = y1;
@@ -16,17 +13,14 @@ int extended_euclidean_algorithm(int a, int b, int& x, int& y) {
 
     return gcd;
 }
-
 int find_modular_inverse(int a, int m) {
     int x, y;
     int gcd = extended_euclidean_algorithm(a, m, x, y);
     if (gcd != 1) {
         throw std::invalid_argument("Inverse does not exist.");
     }
-
     return (x % m + m) % m;
 }
-
 int main() {
     int a, m;
 
@@ -41,6 +35,5 @@ int main() {
     } catch (const std::invalid_argument& e) {
         cout << "Error: " << e.what() << endl;
     }
-
     return 0;
 }
